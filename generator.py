0ea38d4c239e612c17dce77069b389f03e657811
Matan Lachmish <matan.lachmish@jivesoftware.com>
2016-02-19 13:30:24 +0200

    generator v1

---
+++ generator.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+__author__ = "Matan Lachmish and Asaf Rokach"
+__copyright__ = "Copyright 2016"
+__version__ = "1.2"
+__status__ = "Development"
+
+import sys
+import os
+import random
+import string
+
+def main(argv):
+    # Usage check
+    if 3 != len(argv):
+        print "Usage: python %s [localizable source file] [output file]" % argv[0]
+        return -1
+
+    print ''
+    print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    print '~~~ Welcome to Localization tester for iOS ~~~'
+    print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    print ''
+
+    print 'Preparing localizable file',
+    try:
+        inputFileName = argv[1];
+        inputFile = open(inputFileName, 'r')
+    except IOError:
+        print 'Cannot open ' + inputFileName
+        return -1
+
+    try:
+        outputFileName = argv[2];
+        outputFile = open(outputFileName, 'w')
+    except IOError:
+        print 'Cannot open ' + outputFile
+        return -1
+
+    printDone()
+
+    inputLines = inputFile.readlines()
+    for inputLine in inputLines:
+        line_split = inputLine.split('=')
+        if len(line_split) == 2:
+            newString = ''.join(random.choice(string.ascii_letters + string.octdigits + ' ?!:+-') for _ in range(2*len(line_split[1])))
+            line_split[1] = '"' + newString + '";\n';
+        outputFile.write('='.join(line_split))
+
+    inputFile.close()
+    outputFile.close()
+
+
+def printDone():
+    print '... Done'
+
+def printProgress(percentage):
+    sys.stdout.write("\r%d%%" % percentage)
+    sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
