dc8683ea53ec37777323090d7580b8a5d7328177
Matan Lachmish <matan.lachmish@jivesoftware.com>
2016-07-21 10:10:34 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,101 +1,12 @@
-import codecs
-import os
-import sys
+#!/usr/bin/env python
 
-from distutils.util import convert_path
-from fnmatch import fnmatchcase
-from setuptools import setup, find_packages
+from distutils.core import setup
 
-
-def read(fname):
-    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-standard_exclude = ["*.py", "*.pyc", "*$py.class", "*~", ".*", "*.bak"]
-standard_exclude_directories = [
-    ".*", "CVS", "_darcs", "./build", "./dist", "EGG-INFO", "*.egg-info"
-]
-
-def find_package_data(
-    where=".",
-    package="",
-    exclude=standard_exclude,
-    exclude_directories=standard_exclude_directories,
-    only_in_packages=True,
-    show_ignored=False):
-
-    out = {}
-    stack = [(convert_path(where), "", package, only_in_packages)]
-    while stack:
-        where, prefix, package, only_in_packages = stack.pop(0)
-        for name in os.listdir(where):
-            fn = os.path.join(where, name)
-            if os.path.isdir(fn):
-                bad_name = False
-                for pattern in exclude_directories:
-                    if (fnmatchcase(name, pattern)
-                        or fn.lower() == pattern.lower()):
-                        bad_name = True
-                        if show_ignored:
-                            print >> sys.stderr, (
-                                "Directory %s ignored by pattern %s"
-                                % (fn, pattern))
-                        break
-                if bad_name:
-                    continue
-                if (os.path.isfile(os.path.join(fn, "__init__.py"))
-                    and not prefix):
-                    if not package:
-                        new_package = name
-                    else:
-                        new_package = package + "." + name
-                    stack.append((fn, "", new_package, False))
-                else:
-                    stack.append((fn, prefix + name + "/", package, only_in_packages))
-            elif package or not only_in_packages:
-                # is a file
-                bad_name = False
-                for pattern in exclude:
-                    if (fnmatchcase(name, pattern)
-                        or fn.lower() == pattern.lower()):
-                        bad_name = True
-                        if show_ignored:
-                            print >> sys.stderr, (
-                                "File %s ignored by pattern %s"
-                                % (fn, pattern))
-                        break
-                if bad_name:
-                    continue
-                out.setdefault(package, []).append(prefix+name)
-    return out
-
-
-PACKAGE = ""
-NAME = ""
-DESCRIPTION = ""
-AUTHOR = ""
-AUTHOR_EMAIL = ""
-URL = ""
-VERSION = __import__(PACKAGE).__version__
-
-
-setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=read("README.md"),
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    license="BSD",
-    url=URL,
-    packages=find_packages(exclude=["tests.*", "tests"]),
-    package_data=find_package_data(PACKAGE, only_in_packages=False),
-    classifiers=[
-        "Development Status :: Production",
-        "Environment :: iOS Environment",
-        "Intended Audience :: Developers",
-        "License :: MIT License",
-        "Operating System :: OS X",
-        "Programming Language :: Python",
-    ],
-    zip_safe=False,
-)
+setup(name='gibbrish',
+      version='1.0.0',
+      description="Generate '.strings' file with long gibberish strings in order to test if your app is ready for translation.",
+      author='Matan Lachmish',
+      author_email='matan.lachmish@gmail.com',
+      url='https://github.com/mlachmish/gibberish',
+      packages=[],
+     )
